a60cb5a38b77acad99a7a656d3426a86a23f811b
steeleb <steeleb@caryinstitute.org>
2021-06-25 16:14:38 -0600

    unimodality puzzle... solved?
    fingers crossed

--- scripts/LS unimodality test.R
+++ scripts/LS unimodality test.R
@@ -1,9 +1,6 @@
 # this file loads in the histograms from the colab output; creates histos, and then assesses unimodality
 
 library(tidyverse)
-library(diptest)
-library(multimode)
-library(parameters)
 
 final_theme=theme_bw() +
   theme(axis.text=element_text(size=12),
@@ -31,49 +28,47 @@ sun_n_pixels <- 23640
 image_pixels <- temp_histo %>% 
   group_by(date) %>% 
   summarise(tot_pix = sum(count)) %>% 
-  mutate(perc_lake = tot_pix/sun_n_pixels)
+  mutate(perc_lake = round((tot_pix/sun_n_pixels)*100, digits = 2))
 
 #filter for counts that aren't null
 temp_histo_all <- temp_histo %>% 
   filter(count > 0)
+
 #make a list of all dates
 datelist <- unique(temp_histo_all$date)
 date_modal <- as.data.frame(datelist) %>% 
-  mutate(dip_statistic = NA_real_,
-         dip_pval = NA_real_,
-         acr_statistic = NA_real_,
-         acr_pval = NA_real_) %>% 
-  rename(date = datelist)
+  rename(date = datelist) %>% 
+  mutate(unimodal = '')
 
-
+#determine unimodality from density plot, if one derivative change, unimodal if more it's not.
 for(j in 1:length(datelist)) {
   temp_histo_date <- temp_histo_all %>% 
     filter(date == datelist[j])
-  for(i in 1:nrow(temp_histo_date)){
-    if (i == 1) {
-      temp_degC <- NULL
-      temp_degC <- rep(temp_histo_date[i,1], temp_histo_date$count[i])
-    } else {
-      tmp <- NULL
-      tmp <- rep(temp_histo_date[i,1], temp_histo_date$count[i])
-      temp_degC = c(temp_degC,tmp)
-    }
+  temp_histo_date$diff <- NA_real_
+  temp_histo_date$simple_change <- ''
+  for(i in 2:nrow(temp_histo_date)){
+      temp_histo_date$diff[i] = temp_histo_date$count[i] - temp_histo_date$count[i-1]
   }
-  dip <- modetest(temp_degC, method = 'HH')
-  date_modal$dip_statistic[j] = dip$statistic
-  date_modal$dip_pval[j] = dip$p.value
-  acr <- modetest(temp_degC, method = 'ACR')
-  date_modal$acr_statistic[j] = acr$statistic
-  date_modal$acr_pval[j] = acr$p.value
+      temp_histo_date <- temp_histo_date %>% 
+        filter(abs(diff) > 0.01*sun_n_pixels) #difference has to be more than 1% of pixels for a meaningful multimodality
+      if (nrow(temp_histo_date)>2) {
+        for(k in 2:nrow(temp_histo_date)){
+          temp_histo_date$simple_change[k] = case_when(temp_histo_date$diff[k] >0 & temp_histo_date$diff[k-1]< 0 ~ 'y',
+                                                   temp_histo_date$diff[k] <0 & temp_histo_date$diff[k-1]> 0 ~ 'y',
+                                                   TRUE~ NA_character_)
+        }
+        n_change <- nrow(temp_histo_date %>% 
+                           filter(simple_change == 'y'))
+        date_modal$unimodal[j] = case_when(n_change > 1 ~ 'not unimodal',
+                                     TRUE ~ 'unimodal')
+      } else{
+        date_modal$unimodal[j] = 'unk'
+      }
+     
 }
 
-write.csv(date_modal, file.path(datadir, 'date_modalanalysis.csv'), row.names = F)
-
-ggplot(date_modal, aes(x = date, y = dip_pval)) +
-  geom_hline(yintercept = 0.05) +
-  geom_point() +
-  geom_point(aes(x=date, y= acr_pval)) +
-  final_theme
+full_join(date_modal, image_pixels) %>% 
+  write.csv(., file.path(datadir, 'date_modalanalysis.csv'), row.names = F)
 
 histo_unique_data <- full_join(temp_histo_all, image_pixels) %>% 
   full_join(., date_modal)
@@ -92,8 +87,7 @@ for(i in 1:length(datelist)){
          x = 'LS-derived skin temperature (deg C)',
          y = 'count') +
     annotate('text', label = paste0(a$perc_lake[1], ' percent of lake in LS scene'), x = min(a$temp_degC), y = max(a$count), hjust = 0)+
-    annotate('text', label = paste0(a$acr_pval[1], ' ACR p-value'), x = min(a$temp_degC), y = max(a$count)*.9, hjust = 0) +
-    annotate('text', label = paste0(a$dip_pval[1], ' HH p-value'), x = min(a$temp_degC), y = max(a$count)*.8, hjust = 0) 
+    annotate('text', label = paste0(a$unimodal[1]), x = min(a$temp_degC), y = max(a$count)*.9, hjust = 0)
   print(hist)
 }
 
